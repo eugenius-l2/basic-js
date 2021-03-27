edae2c9a9be01b39d9f5bdd7d1f46b7286d74137
Eugene Bobrovnik <eugene.bobrovnik@gmail.com>
2021-03-27 20:50:56 +0300

    feat: add my solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const flagsNext = ['--discard-next', '--double-next'];
+  const flagsPrev = ['--discard-prev', '--double-prev' ];
+  let tmp = [];
+  let resultArray = [];
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === flagsNext[0]) {
+        tmp.push('magic');
+        i++;
+      } else if (arr[i] === flagsNext[1]) {
+        tmp.push('magic');
+        if (typeof arr[i + 1] !== 'undefined') tmp.push(arr[i + 1])
+      } else {
+        tmp.push(arr[i]);
+      }
+    }
+    tmp.reverse();
+    for (let i = 0; i < tmp.length; i++) {
+      if (tmp[i] === flagsPrev[0]) {
+        resultArray.push('magic');
+        i++;
+      } else if (tmp[i] === flagsPrev[1]) {
+        resultArray.push('magic');
+        if (typeof tmp[i + 1] !== 'undefined') resultArray.push(tmp[i + 1])
+      } else {
+        resultArray.push(tmp[i]);
+      }
+    }
+  } else {
+    throw 'Not an array!'
+  }
+  resultArray.reverse();
+  return resultArray.filter(item => item !== 'magic');
 };
